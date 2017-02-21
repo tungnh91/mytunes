fe94bbd405b7fd8b4b81f4bd99d5a2ddfcb61b6b
tungnh91 <tungnh91@gmail.com>
2017-02-20 18:54:23 -0800

    Passed tests for Song Queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,32 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
 
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      if (this.length > 1) {
+        this.shift();
+        this.playFirst();
+      } else if (this.length === 1) {
+        this.shift();
+      }
+    });
+
+    this.on('dequeue', function(id) {
+      this.remove(id);
+    });
+  },
+
+  playFirst: function() {
+    if (this.length >= 1) {
+      this.at(0).play();
+    }
+  },
+
+  
 });
