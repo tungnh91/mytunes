d959af85fe1b9e97cf9c09156d57b23f4e346d0d
tungnh91 <tungnh91@gmail.com>
2017-02-20 19:13:46 -0800

    "Passed tests for SongQueueView"

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,6 +2,9 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    
+    this.collection.on('change', SongQueueEntryView.prototype.render());
+    this.collection.on('change', this.render());
   },
 
   render: function() {
