5fbd7275e301f092d7aa829cd86e618371c8de8a
tungnh91 <tungnh91@gmail.com>
2017-02-21 19:12:57 -0800

    Added a sweet emoji play count

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,12 +3,15 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td> <strong>Play Count:</strong> <%= count %></td>'),
 
   events: {
     'click': function() {
       this.model.play();
       this.model.enqueue();
+      var num = this.model.get('count');
+      this.model.set('count', num + '<i class="em em-shipit"></i>');
+      this.render();
     }
   },
 
